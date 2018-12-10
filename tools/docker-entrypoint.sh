91cec6fcdb36c245b40b91aab67bdd53dc4ce083
Kukks <evilkukka@gmail.com>
2018-12-10 09:21:15 +0100

    fix network naming in entrypoint

--- tools/docker-entrypoint.sh
+++ tools/docker-entrypoint.sh
@@ -5,7 +5,16 @@
 if [[ -z "${LIGHTNINGD_OPT}" ]]; then
   echo "${LIGHTNINGD_OPT}" > $LIGHTNINGD_DATA/config
 fi
-
+NETWORK=$(sed -n 's/^network=\(.*\)$/\1/p' < "$LIGHTNINGD_DATA/config")
+REPLACEDNETWORK="";
+if [ "$NETWORK" == "mainnet" ]; then
+	REPLACEDNETWORK="groestlcoin"
+fi
+if [[ $REPLACEDNETWORK ]]; then
+    sed -i '/^network=/d' "$LIGHTNINGD_DATA/config"
+    echo "network=$REPLACEDNETWORK" >> "$LIGHTNINGD_DATA/config"
+    echo "Replaced network $NETWORK by $REPLACEDNETWORK in $LIGHTNINGD_DATA/config"
+fi
 
 if [ "$EXPOSE_TCP" == "true" ]; then
     set -m
